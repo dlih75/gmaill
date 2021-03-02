d1abbe1df658332ce6b3dfd0883010e980a98ecb
hmjjj <74424096+dlih75@users.noreply.github.com>
2021-03-02 16:56:44 +0300

    Add files via upload

---
+++ ZIRO.py
@@ -0,0 +1,27 @@
+import time
+import re
+import os
+import random
+
+
+logo = """
+
+AUTHOR:ZIRO
+Telegram@XZIRO12
+
+"""
+os.system("figlet ZIRO")
+print(logo)
+def lewan():
+    name=input("Naw :")
+    url=int(input("gmail chan bet :"))
+    for x in range(url):
+        num='123456789056720938574193458185665919267685483929011101102929384847556'
+        a1=random.choice(num)
+        a2=random.choice(num)
+        a3=random.choice(num)
+        lewan=name+a1+a2+a3+"@gmail.com"
+        print(lewan)
+        file.write(lewan+"\n")
+        
+lewan()
